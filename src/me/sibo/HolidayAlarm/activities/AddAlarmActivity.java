939833f9b34f4d6ad7979f73be1cb26f0ab22784
yangsibai <yangsibai@gmail.com>
2014-10-08 00:22:16 +0800

    add header in new alarm activity

--- src/me/sibo/HolidayAlarm/activities/AddAlarmActivity.java
+++ src/me/sibo/HolidayAlarm/activities/AddAlarmActivity.java
@@ -1,16 +1,31 @@
 package me.sibo.HolidayAlarm.activities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import me.sibo.HolidayAlarm.R;
 
 /**
  * Created by massimo on 14-10-7.
  */
 public class AddAlarmActivity extends Activity {
+    private ImageButton backBtn;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_alarm);
+
+        backBtn = (ImageButton)this.findViewById(R.id.ib_back);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(AddAlarmActivity.this, HomeActivity.class);
+                AddAlarmActivity.this.startActivity(intent);
+            }
+        });
     }
 }
