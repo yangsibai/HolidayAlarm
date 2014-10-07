205006de8bb625c972591d41a3551e7f5d60322a
yangsibai <yangsibai@gmail.com>
2014-10-07 17:09:20 +0800

    change listview to relative layout

--- src/me/sibo/HolidayAlarm/activities/HomeActivity.java
+++ src/me/sibo/HolidayAlarm/activities/HomeActivity.java
@@ -10,29 +10,15 @@ import android.widget.Toast;
 import me.sibo.HolidayAlarm.R;
 import me.sibo.HolidayAlarm.adapters.SeparatedListAdapter;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class HomeActivity extends Activity {
 
-    public final static String ITEM_TITLE = "title";
-    public final static String ITEM_CAPTION = "caption";
+    private final static String[] days = new String[]{"今天", "明天", "后天", "以后"};
 
-    private final static String[] days = new String[]{"Mon", "Tue", "Web", "Thur", "Fri"};
+    private final static String[] notes =new String[]{"08:00", "08:10", "08:20"};
 
-    private final static String[] notes =new String[]{"Ate Breakfast", "Ran a Marathan ...yah really", "Slept all day"};
-
-    private ListView addAlarmEntryItem;
     private SeparatedListAdapter adapter;
 
     private ListView alarmListView;
-
-    public Map<String, ?> createItem(String title, String caption){
-        Map<String, String> item = new HashMap<String, String>();
-        item.put(ITEM_TITLE, title);
-        item.put(ITEM_CAPTION, caption);
-        return item;
-    }
 
     /**
      * Called when the activity is first created.
@@ -41,18 +27,6 @@ public class HomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-       final ArrayAdapter<String> alarmEntryAdapter = new ArrayAdapter<String>(this, R.layout.add_alarm, new String[]{"Add alarm"});
-
-        addAlarmEntryItem = (ListView)this.findViewById(R.id.add_alarm);
-        addAlarmEntryItem.setAdapter(alarmEntryAdapter);
-        addAlarmEntryItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = alarmEntryAdapter.getItem(position);
-                Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
-            }
-        });
 
         //create the listview adapter
         adapter = new SeparatedListAdapter(this);
